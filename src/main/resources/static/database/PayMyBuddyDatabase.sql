697d7031eec9bfeae717bde956c43b6f101e008f
MrgnCpn <morgancpn.dev@gmail.com>
2020-07-10 19:09:36 +0200

    Start database script, write tables creations

---
+++ src/main/resources/static/database/PayMyBuddyDatabase.sql
@@ -0,0 +1,90 @@
+-- CREATE Database
+    DROP DATABASE IF EXISTS PayMyBuddy_OC_MC;
+    CREATE DATABASE IF NOT EXISTS PayMyBuddy_OC_MC CHARACTER SET 'utf8';
+    USE PayMyBuddy_OC_MC;
+    SET autocommit=1;
+
+-- CREATE Tables
+    DROP TABLE IF EXISTS users;
+    DROP TABLE IF EXISTS transactions;
+    DROP TABLE IF EXISTS bankAccount;
+    DROP TABLE IF EXISTS buddyAccount;
+
+    -- Users Table
+        CREATE TABLE users (
+            id INT PRIMARY KEY AUTO_INCREMENT,
+            firstname VARCHAR(60) NOT NULL,
+            lastname VARCHAR(60) NOT NULL,
+            birthday DATETIME NOT NULL,
+            phone VARCHAR(25),
+            email VARCHAR(60) NOT NULL,
+            password VARCHAR(250) NOT NULL,
+            country_code CHAR(3),
+            UNIQUE INDEX index_email (email)
+        )
+        ENGINE=INNODB
+        AUTO_INCREMENT=0;
+
+    -- Transactions Table
+        CREATE TABLE transactions (
+            id INT PRIMARY KEY AUTO_INCREMENT,
+            from_id INT NOT NULL,
+            to_id INT NOT NULL,
+            date DATETIME NOT NULL,
+            description TEXT,
+            amount INT NOT NULL,
+            currency CHAR(3) NOT NULL,
+            CONSTRAINT FK_from_id FOREIGN KEY (from_id) REFERENCES users(id),
+            CONSTRAINT FK_to_id FOREIGN KEY (to_id) REFERENCES users(id)
+        )
+        ENGINE=INNODB
+        AUTO_INCREMENT=0;
+
+    -- BankAccount Table
+        CREATE TABLE bankAccount (
+            id INT PRIMARY KEY AUTO_INCREMENT,
+            user_id INT NOT NULL,
+            type CHAR(1) NOT NULL,
+            card_number LONG,
+            card_cryptogramme INT(3),
+            iban VARCHAR(34),
+            bic VARCHAR(11),
+            wording VARCHAR(150),
+            CONSTRAINT FK_userId_id_bankAccount FOREIGN KEY (user_id) REFERENCES users(id)
+        )
+        ENGINE=INNODB
+        AUTO_INCREMENT=0;
+
+    -- BuddyAccount Table
+        CREATE TABLE buddyAccount (
+            id INT PRIMARY KEY AUTO_INCREMENT,
+            user_id INT NOT NULL,
+            type CHAR(1) NOT NULL,
+            ammount INT NOT NULL,
+            currency CHAR(3),
+            CONSTRAINT FK_userId_id_buddyAccount FOREIGN KEY (user_id) REFERENCES users(id)
+        ) ENGINE=INNODB
+        AUTO_INCREMENT=0;
+
+-- INSERT data
+    LOCK TABLES users WRITE;
+        -- INSERT
+    UNLOCK TABLES;
+
+    LOCK TABLES transactions WRITE;
+        -- INSERT
+    UNLOCK TABLES;
+
+    LOCK TABLES bankAccount WRITE;
+        -- INSERT
+    UNLOCK TABLES;
+
+    LOCK TABLES buddyAccount WRITE;
+        -- INSERT
+    UNLOCK TABLES;
+
+-- Procedures
+
+
+-- Triggers
+
