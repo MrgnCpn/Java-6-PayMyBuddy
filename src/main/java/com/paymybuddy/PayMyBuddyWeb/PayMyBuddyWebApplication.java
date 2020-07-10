88ed39ef379d4b32f608d3bfe20a222b6aa24306
MrgnCpn <morgancpn.dev@gmail.com>
2020-07-10 13:33:28 +0200

    Init project

---
+++ src/main/java/com/paymybuddy/PayMyBuddyWeb/PayMyBuddyWebApplication.java
@@ -0,0 +1,13 @@
+package com.paymybuddy.PayMyBuddyWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayMyBuddyWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayMyBuddyWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/paymybuddy/PayMyBuddyWeb/PayMyBuddyWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paymybuddy.PayMyBuddyWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayMyBuddyWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
