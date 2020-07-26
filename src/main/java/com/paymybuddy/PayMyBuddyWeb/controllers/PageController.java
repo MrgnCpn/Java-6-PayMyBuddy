f5bef3009925673605c9fbc257cbf08d5ba805f4
MrgnCpn <morgancpn.dev@gmail.com>
2020-07-26 22:15:15 +0200

    Code Refactor

--- src/main/java/com/paymybuddy/PayMyBuddyWeb/controllers/PageController.java
+++ src/main/java/com/paymybuddy/PayMyBuddyWeb/controllers/PageController.java
@@ -1,6 +1,7 @@
 package com.paymybuddy.PayMyBuddyWeb.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.RedirectView;
@@ -12,99 +13,19 @@ import java.util.Map;
 @RestController
 @Singleton
 public class PageController {
+
     @GetMapping("/")
     public ModelAndView root(){
         RedirectView redirectView = new RedirectView();
-        redirectView.setUrl("/account");
+        redirectView.setUrl("/login");
 
         return new ModelAndView(redirectView);
     }
 
-    @GetMapping("/login")
-    public ModelAndView login(){
+    @GetMapping("/{page}")
+    public ModelAndView rooter(@PathVariable(value="page") String page){
         Map<String, Object> model = new HashMap<>();
-        model.put("page", "login");
-        model.put("isLogin", false);
-
-        return new ModelAndView("template.html" , model);
-    }
-
-    @GetMapping("/signup")
-    public ModelAndView signup(){
-        Map<String, Object> model = new HashMap<>();
-        model.put("page", "signup");
-        model.put("isLogin", false);
-
-        return new ModelAndView("template.html" , model);
-    }
-
-    @GetMapping("/account")
-    public ModelAndView account(){
-        Map<String, Object> model = new HashMap<>();
-        model.put("page", "account");
-        model.put("isLogin", true);
-
-        return new ModelAndView("template.html" , model);
-    }
-
-    @GetMapping("/account/manageCards/addNewCard")
-    public ModelAndView addNewCard(){
-        Map<String, Object> model = new HashMap<>();
-        model.put("page", "addNewCard");
-        model.put("isLogin", true);
-
-        return new ModelAndView("template.html" , model);
-    }
-
-    @GetMapping("/transfer")
-    public ModelAndView transfer(){
-        Map<String, Object> model = new HashMap<>();
-        model.put("page", "transfer");
-        model.put("isLogin", true);
-
-        return new ModelAndView("template.html" , model);
-    }
-
-    @GetMapping("/transfer/passwordCheck")
-    public ModelAndView passwordCheck(){
-        Map<String, Object> model = new HashMap<>();
-        model.put("page", "passwordCheck");
-        model.put("isLogin", true);
-
-        return new ModelAndView("template.html" , model);
-    }
-
-    @GetMapping("/profile")
-    public ModelAndView profile(){
-        Map<String, Object> model = new HashMap<>();
-        model.put("page", "profile");
-        model.put("isLogin", true);
-
-        return new ModelAndView("template.html" , model);
-    }
-
-    @GetMapping("/profile/changePassword")
-    public ModelAndView changePassword(){
-        Map<String, Object> model = new HashMap<>();
-        model.put("page", "changePassword");
-        model.put("isLogin", true);
-
-        return new ModelAndView("template.html" , model);
-    }
-
-    @GetMapping("/contact")
-    public ModelAndView contact(){
-        Map<String, Object> model = new HashMap<>();
-        model.put("page", "contact");
-        model.put("isLogin", true);
-
-        return new ModelAndView("template.html" , model);
-    }
-
-    @GetMapping("/contact/profile")
-    public ModelAndView contactProfile(){
-        Map<String, Object> model = new HashMap<>();
-        model.put("page", "contactProfile");
+        model.put("page", page);
         model.put("isLogin", true);
 
         return new ModelAndView("template.html" , model);
