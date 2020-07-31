c037526f20b36b7360a5842207b8a9b9463afed1
MrgnCpn <morgancpn.dev@gmail.com>
2020-07-31 16:31:50 +0200

    Create DatabaseConfiguration class and properties file

---
+++ src/main/java/com/paymybuddy/PayMyBuddyWeb/configuration/DatabaseConfiguration.java
@@ -0,0 +1,112 @@
+package com.paymybuddy.PayMyBuddyWeb.configuration;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.*;
+import java.util.Properties;
+
+public class DatabaseConfiguration {
+
+    /**
+     * Logger log4j2
+     */
+    private static final Logger logger = LogManager.getLogger("DatabaseConfiguration");
+
+    /**
+     * Database host
+     */
+    private String host;
+    /**
+     * Database port
+     */
+    private String port;
+    /**
+     * Database database name
+     */
+    private String database;
+    /**
+     * Database username
+     */
+    private String user;
+    /**
+     * Database password
+     */
+    private String password;
+
+    /**
+     * Open Connection on OC_parkingSystem_p4_prod DB
+     * @return Connection
+     * @throws ClassNotFoundException
+     * @throws SQLException
+     */
+    public Connection getConnection() throws ClassNotFoundException, SQLException {
+        logger.info("Create DB connection");
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        try (InputStream inputStream = new FileInputStream("src/main/resources/static/database/databaseConfiguration.properties")){
+            Properties properties = new Properties();
+            properties.load(inputStream);
+            host = properties.getProperty("host");
+            port = properties.getProperty("port");
+            database = properties.getProperty("database");
+            user = properties.getProperty("user");
+            password = properties.getProperty("password");
+        } catch (FileNotFoundException e) {
+            logger.error("File not found", e);
+        } catch (IOException e) {
+            logger.error("Error while read file", e);
+        }
+        return DriverManager.getConnection(
+                "jdbc:mysql://" + host + ":" + port + "/" + database, user, password);
+    }
+
+    /**
+     * Close Connection
+     * @param con Active connection
+     */
+    public void closeConnection(Connection con) {
+        if (con != null) {
+            try {
+                con.close();
+                logger.info("Closing DB connection");
+            } catch (SQLException e) {
+                logger.error("Error while closing connection", e);
+            }
+        }
+    }
+
+    /**
+     * Close Prepared Statement
+     * @param ps Open statement
+     */
+    public void closePreparedStatement(PreparedStatement ps) {
+        if (ps != null) {
+            try {
+                ps.close();
+                logger.info("Closing Prepared Statement");
+            } catch (SQLException e) {
+                logger.error("Error while closing prepared statement", e);
+            }
+        }
+    }
+
+
+    /**
+     * Close ResultSet
+     * @param rs Open ResultSet
+     */
+    public void closeResultSet(ResultSet rs) {
+        if (rs != null) {
+            try {
+                rs.close();
+                logger.info("Closing Result Set");
+            } catch (SQLException e) {
+                logger.error("Error while closing result set", e);
+            }
+        }
+    }
+}
